102282c4958cff3425832c77ca783b8419f0eb5b
Parthlad1 <parthlad153@gmail.com>
2026-09-05 23:06:59 +0530

    Add backend health endpoint

---
+++ src/main/java/com/parthlad/portfolio_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.parthlad.portfolio_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Portfolio backend is running.";
+    }
+}
